56153b09debe0d236fbd3ac212d32b09962fb185
Aditya Killekar <killekaraditya@gmail.com>
2018-04-17 15:35:58 -0700

    fixed split files function

--- csv_read.py
+++ csv_read.py
@@ -6,22 +6,20 @@ unknown_feature_headers = ['job','marital','default','housing','loan']
 
 def split_files():
     df = pd.read_csv('bank-additional.csv')
-    df_filter_test = [(df['job'] == 'unknown'), (df['marital'] == 'unknown'), (df['default'] == 'unknown'),\
+    df_filter_test = [(df['job'] == 'unknown'), (df['marital'] == 'unknown'), (df['housing'] == 'unknown'), (df['default'] == 'unknown'),\
                  (df['housing'] == 'unknown'), (df['loan'] == 'unknown')]
     df_filter_train = [(df['job'] != 'unknown'), (df['marital'] != 'unknown'), (df['default'] != 'unknown'),\
                  (df['housing'] != 'unknown'), (df['loan'] != 'unknown')]
 
-    job_unknown_df = df[(df['job'] == 'unknown')]
-    job_unknown_df = job_unknown_df[['age','campaign','pdays','previous','poutcome','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y','job']]
-    # for i,j in zip(df_filter_train,unknown_headers):
-    #     job_unknown_df = df[i]
-    #     job_unknown_df[['age','campaign','pdays','previous','poutcome','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y',j]].to_csv('train_'+j+'.csv')
-    #
-    # for i,j in zip(df_filter_test,unknown_headers):
-    #     job_unknown_df = df[i]
-    #     job_unknown_df[['age','campaign','pdays','previous','poutcome','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y']].to_csv('test_'+j+'.csv')
-    print(job_unknown_df)
-    return job_unknown_df
+    # job_unknown_df = df[(df['job'] == 'unknown')]
+    # job_unknown_df = job_unknown_df[['age','campaign','pdays','previous','poutcome','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y','job']]
+    for i,j in zip(df_filter_train,unknown_feature_headers):
+        job_unknown_df = df[i]
+        job_unknown_df[['age','campaign','pdays','previous','poutcome','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y',j]].to_csv('train_'+j+'.csv')
+
+    for i,j in zip(df_filter_test,unknown_feature_headers):
+        job_unknown_df = df[i]
+        job_unknown_df[['age','campaign','pdays','previous','poutcome','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y']].to_csv('test_'+j+'.csv')
 
 def one_hot_encode_features(data):
     poutcome_features = data.pop('poutcome')
@@ -37,8 +35,8 @@ def one_hot_encode_features(data):
 
 
 def main():
-    data = split_files()
-    one_hot_encode_features(data)
+    split_files()
+    # one_hot_encode_features(data)
 
 if __name__ == '__main__':
     main()
