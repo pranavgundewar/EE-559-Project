c0749cb1fb08c81fe81944b65b08dc293b910362
Aditya Killekar <killekaraditya@gmail.com>
2018-04-17 15:22:43 -0700

    My first commit

---
+++ Project_Part1.py
@@ -0,0 +1,80 @@
+"""
+Final Project for EE 559 Spring 2018
+Program by :
+@authors   : Pranav Gundewar    Aditya Killekar
+USC ID     : 4463612994         2051450417
+Email      : gundewar@usc.edu   killekar@usc.edu
+Dataset    : bank-additional.csv
+Instructor : Professor Keith Jenkins
+"""
+# Importing Libraries
+import numpy as np
+import pandas as pd
+from pandas import Series
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import label_binarize
+
+# Data loading and preprocessing
+df = pd.read_csv('bank-additional.csv', delimiter = ',')
+#term_deposits = df.copy()
+# Have a grasp of how our data looks.
+#print(df.dtypes)
+
+
+# Pre-procesing the data
+"""
+If there were missing values we will have to fill them with the median, mean or mode.
+I prefer to use the median as it works better with large datasets
+"""
+# No missing values ?
+df = df.replace(to_replace=['unknown'], value = np.NaN , regex = True)
+#print(df.info())
+#print(df.describe())
+
+# Replace missing data marked as unknown to NaN to use imputation to fill the missing data
+
+#Imputer
+#df['housing'] = df['housing'].fillna(df['housing'].mode()[0])
+
+## forward-fill
+#df = df.fillna(method='ffill')
+#
+## back-fill
+#df = df.fillna(method='bfill')
+
+cols = df.columns[df.isna().any()].tolist()
+print('Columns that has missing values:',cols)
+df[cols] = df[cols].fillna(df.mode().iloc[0])
+
+# Let's see how the numeric data is distributed.
+#df.hist(bins=10, figsize=(12,12), color='#E14906')
+#plt.show()
+
+# Binarize the columns
+# Convert the columns that contain a Yes or No. (Binary Columns)
+def convert_to_int(df, new_column, target_column):
+    df[new_column] = df[target_column].apply(lambda x: 0 if x == 'no' else 1)
+    return df[new_column].value_counts()
+
+convert_to_int(df, "y", "y") #Create a deposit int
+convert_to_int(df, "housing", "housing") # Create housingint column
+convert_to_int(df, "loan", "loan") #Create a loan_int column
+convert_to_int(df, "default", "default") #Create a default_int column
+c = Series.unique(df['contact'])
+df['contact'] = label_binarize(df['contact'], classes=c)
+
+
+def one_hot(df, cols):
+    """
+    @param df pandas DataFrame
+    @param cols a list of columns to encode
+    @return a DataFrame with one-hot encoding
+    """
+    for each in cols:
+        dummies = pd.get_dummies(df[each], prefix=each, drop_first=True)
+        df = pd.concat([df, dummies], axis=1)
+    return df
+cols = ['job', 'marital', 'education']
+print('Columns to be one-hot encoded:',cols)
+df1 = one_hot(df, cols)
+df1.to_csv('onehot.csv')
---
+++ csv_read.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import re
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+
+unknown_feature_headers = ['job','marital','default','housing','loan']
+
+def split_files():
+    df = pd.read_csv('bank-additional.csv')
+    df_filter_test = [(df['job'] == 'unknown'), (df['marital'] == 'unknown'), (df['default'] == 'unknown'),\
+                 (df['housing'] == 'unknown'), (df['loan'] == 'unknown')]
+    df_filter_train = [(df['job'] != 'unknown'), (df['marital'] != 'unknown'), (df['default'] != 'unknown'),\
+                 (df['housing'] != 'unknown'), (df['loan'] != 'unknown')]
+
+    job_unknown_df = df[(df['job'] == 'unknown')]
+    job_unknown_df = job_unknown_df[['age','campaign','pdays','previous','poutcome','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y','job']]
+    # for i,j in zip(df_filter_train,unknown_headers):
+    #     job_unknown_df = df[i]
+    #     job_unknown_df[['age','campaign','pdays','previous','poutcome','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y',j]].to_csv('train_'+j+'.csv')
+    #
+    # for i,j in zip(df_filter_test,unknown_headers):
+    #     job_unknown_df = df[i]
+    #     job_unknown_df[['age','campaign','pdays','previous','poutcome','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y']].to_csv('test_'+j+'.csv')
+    print(job_unknown_df)
+    return job_unknown_df
+
+def one_hot_encode_features(data):
+    poutcome_features = data.pop('poutcome')
+    label_encoder = LabelEncoder()
+    one_hot_encoder = OneHotEncoder(sparse=False)
+    main_poutcome_features = label_encoder.fit_transform(poutcome_features)
+    main_poutcome_features = main_poutcome_features.reshape(
+        len(main_poutcome_features), 1)
+    main_poutcome_features = pd.DataFrame(
+        one_hot_encoder.fit_transform(main_poutcome_features)
+    )
+    print(main_poutcome_features)
+
+
+def one_hot(df, cols):
+    """
+    @param df pandas DataFrame
+    @param cols a list of columns to encode
+    @return a DataFrame with one-hot encoding
+    """
+    for each in cols:
+        dummies = pd.get_dummies(df[each], prefix=each, drop_first=True)
+        df = pd.concat([df, dummies], axis=1)
+    return df
+
+
+def main():
+    data = split_files()
+    one_hot_encode_features(data)
+
+    cols = ['poutcome']
+    print('Columns to be one-hot encoded:',cols)
+    df1 = one_hot(data, cols)
+    print(df1)
+
+if __name__ == '__main__':
+    main()
