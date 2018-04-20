559b8e18129f8a4e2678f2c55a8b441bb9e83379
Pranav Gundewar <pranavgundewar@gmail.com>
2018-04-20 01:47:53 -0700

    Classifier_added_Check_handle

--- Project_Part1.py
+++ Project_Part1.py
@@ -10,87 +10,75 @@ Instructor : Professor Keith Jenkins
 # Importing Libraries
 import numpy as np 
 import pandas as pd
-from pandas import Series
-from sklearn.preprocessing import label_binarize
-import random
+import handle_missing_data
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.decomposition import PCA
 
-# Binarize the columns
-# Convert the columns that contain a Yes or No. (Binary Columns)
-def convert_to_int(df, new_column, target_column):
-    df[new_column] = df[target_column].apply(lambda x: 0 if x == 'no' else 1)
-    return df[new_column].value_counts()
-
-def one_hot(df, cols):
+def one_hot(df):
     """
     @param df pandas DataFrame
     @param cols a list of columns to encode 
     @return a DataFrame with one-hot encoding
     """
+    # One-hot encode into 
+    cols = ['job', 'marital', 'education', 'month', 'day_of_week', 'poutcome']
     for each in cols:
         dummies = pd.get_dummies(df[each], prefix=each, drop_first=False)
         df = pd.concat([df, dummies], axis=1)
-    return df
-
-def handle_missing_data(df, method):
-    """
-    :param 
-    :param method: {mode, median, remove, backward filling, forward filling, KNN, SVM}
-    :return: data with filled missing values
-    :missing_handles: fillna with parameters of training set, since it will be unknown for test set
-    """
-
-    if method == 'mode':
-        cols = df.columns[df.isna().any()].tolist()
-        print('Columns that has missing values:',cols)
-        df[cols] = df[cols].fillna(df.mode().iloc[0])
-        
-    elif method == 'ffill':
-        # forward-fill
-        df = df.fillna(method='ffill')
-        
-    elif method == 'bfill':
-        # back-fill
-        df = df.fillna(method='bfill')
-        
-    elif method == 'drop':
-        df=df.dropna(axis=0, how='any') 
-
+    df = df.drop(cols,axis=1)
     return df
 
 #def main():
-#    cols = ['job', 'marital', 'education']
-#    print('Columns to be one-hot encoded:',cols)
-#    # Data loading and preprocessing
-#    df = pd.read_csv('bank-additional.csv', delimiter = ',')
-#    # No missing values ?
-#    df = df.replace(to_replace=['unknown'], value = np.NaN , regex = True)
-#    df = handle_missing_data(df, 'mode')
-#    convert_to_int(df, "y", "y") #Create a deposit int
-#    convert_to_int(df, "housing", "housing") # Create housingint column
-#    convert_to_int(df, "loan", "loan") #Create a loan_int column
-#    convert_to_int(df, "default", "default") #Create a default_int column
-#    c = Series.unique(df['contact'])
-#    df['contact'] = label_binarize(df['contact'], classes=c)
-#    print(df['default'].value_counts())
-#    df1 = one_hot(df, cols)
-#    
-#    
+
 #if __name__ == '__main__':
 #    main()
 
-cols = ['job', 'marital', 'education']
-#print('Columns to be one-hot encoded:',cols)
-# Data loading and preprocessing
-df = pd.read_csv('bank-additional.csv', delimiter = ',')
-print(df['housing'].value_counts())
-# No missing values ?
-df = df.replace(to_replace=['unknown'], value = np.NaN , regex = True)
-df = handle_missing_data(df, 'mode')
-convert_to_int(df, "y", "y") #Create a deposit int
-convert_to_int(df, "housing", "housing") # Create housingint column
-convert_to_int(df, "loan", "loan") #Create a loan_int column
-convert_to_int(df, "default", "default") #Create a default_int column
-c = Series.unique(df['contact'])
-df['contact'] = label_binarize(df['contact'], classes=c)
-print(df['housing'].value_counts())
-df1 = one_hot(df, cols)
+print('Pre-Procesing the input data!\n')
+df = handle_missing_data.main('RF')
+df = one_hot(df)
+print('\nPre-Processing Done!\n')
+
+y = df['y'] 
+df = df.drop(columns=['y'], axis=1)
+X = df.drop(columns=['default'], axis=1)
+
+"""
+PCA
+"""
+pca = PCA(n_components=2)
+pca.fit(X)
+X = pca.transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=0, stratify = y)
+
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+X_train_n=scaler.transform(X_train)
+X_test_n=scaler.transform(X_test)
+
+clsr_names=["Nearest Neighbors", "Linear SVM", "RBF SVM",
+ "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+ "Naive Bayes"]
+classifiers = [KNeighborsClassifier(5),
+ SVC(kernel="linear", C=0.025), SVC(gamma=1, C=0.5),
+ DecisionTreeClassifier(max_depth=5),
+ RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+ MLPClassifier(alpha=1),
+ AdaBoostClassifier(),
+ GaussianNB()] 
+
+print("Comparing different models:\n")
+for name, clf in zip(clsr_names, classifiers):
+    model=clf.fit(X_train_n,y_train)
+    y_pred=model.predict(X_test_n)
+    print(name+" Accuracy: {0:.4f}%".format(100*float((y_pred==y_test).sum())/float(len(y_test)))) 
--- handle_missing_data.py
+++ handle_missing_data.py
@@ -11,9 +11,12 @@ import glob
 import os
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-# np.set_printoptions(threshold='nan')
+from pandas import Series
+from sklearn.preprocessing import label_binarize
 
-def handle_missing_data_using_NN(data, test_data, hidden_layer_config):
+
+def handle_missing_data_using_NN(data, test_data):
+    hidden_layer_config = [20, 60, 110, 210, 750, 1850, 660, 450, 210, 20]
     train_data = data[['age','campaign','pdays','previous','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y','poutcome_failure','poutcome_nonexistent','poutcome_success']].values
     train_labels = data[data.columns.difference(['age','campaign','pdays','previous','emp.var.rate','cons.price.idx','cons.conf.idx','euribor3m','nr.employed','y','poutcome_failure','poutcome_nonexistent','poutcome_success'])].values
     train_labels = np.delete(train_labels, 0, 1)
@@ -93,15 +96,16 @@ def handle_missing_data_using_RF(data, test, feature):
 
     for pred, i in zip(predictions, indices):
         filled_df.loc[i, feature] = label_decode[pred]
-
+    
     filled_df.to_csv('bank-additional-preprocessed.csv',index=False)
+    return filled_df
     
 
 def neural_model(input_shape, output_shape, hidden_layer_config=[10, 15]):
     input_layer = Input(shape=(input_shape,))
     network_layers = []
     network_layers.append(input_layer)
-    hidden_layers = len(hidden_layer_config)
+#    hidden_layers = len(hidden_layer_config)
     for hidden_layer_size in hidden_layer_config:
         new_layer = Dense(
             hidden_layer_size,
@@ -129,12 +133,18 @@ def neural_model(input_shape, output_shape, hidden_layer_config=[10, 15]):
 
 def run_network(network, X, Y, X_test, epochs=50):
     # print(train_test_split(X, Y, test_size=0.3))
-    x_train, x_vald, y_train, y_vald = train_test_split(X, Y, test_size=0.3)
+    from sklearn.model_selection import StratifiedShuffleSplit
+    sss = StratifiedShuffleSplit(n_splits=3, test_size=0.3, random_state=0)
+    for train_index, test_index in sss.split(X, Y):
+        X_train, X_vald = X[train_index], X[test_index]
+        Y_train, Y_vald = Y[train_index], Y[test_index]
+    
+#    x_train, x_vald, y_train, y_vald = train_test_split(X, Y, test_size=0.3)
 
-    x_train = np.asarray(x_train)
-    y_train = np.asarray(y_train)
-    x_vald = np.asarray(x_vald)
-    y_vald = np.asarray(y_vald)
+    x_train = np.asarray(X_train)
+    y_train = np.asarray(Y_train)
+    x_vald = np.asarray(X_vald)
+    y_vald = np.asarray(Y_vald)
 
     model_result = network.fit(
         x_train, y_train,
@@ -148,24 +158,59 @@ def run_network(network, X, Y, X_test, epochs=50):
 
     return model_result, predictions
 
-def main():
+# Convert the columns that contain a Yes or No. (Binary Columns)
+def convert_to_int(df, new_column, target_column):
+    df[new_column] = df[target_column].apply(lambda x: 0 if x == 'no' else 1)
+    return df[new_column].value_counts()
+
+def main(method):
     if os.path.isfile('bank-additional-preprocessed.csv'):
         os.remove('bank-additional-preprocessed.csv')
     # df_train = pd.read_csv('train_one_hot_job.csv')
     # df_test = pd.read_csv('test_one_hot_job.csv')
+    df = pd.read_csv('bank-additional.csv', delimiter = ',')
+    df = df.replace(to_replace=['unknown'], value = np.NaN , regex = True)
     globbed_files = glob.glob('train_one_hot_*'+'.csv')
     for csv in globbed_files:
         df_train = pd.read_csv(csv)
         test_csv = csv.split('_')[-1].split('.')[0]
         print('Handling missing data for '+test_csv)
         df_test = pd.read_csv('test_one_hot_'+test_csv+'.csv')
-        handle_missing_data_using_RF(df_train, df_test, test_csv)
-    # handle_missing_data_using_NN(df_train, df_test, [20, 60, 110, 210, 750, 1850, 660, 450, 210, 20])
-    # handle_missing_data_using_SVM(df_train,df_test)
-    # handle_missing_data_using_RF(df_train, df_test, 'job')
+        if method == 'RF':
+            df = handle_missing_data_using_RF(df_train, df_test, test_csv)
+        elif method == 'NN':
+            df = handle_missing_data_using_NN(df_train, df_test)
+        elif method == 'SVM':
+            df = handle_missing_data_using_SVM(df_train,df_test)
+            
+        elif method == 'mode':
+            df = pd.read_csv('bank-additional.csv', delimiter = ',')
+            df = df.replace(to_replace=['unknown'], value = np.NaN , regex = True)
+            cols = df.columns[df.isna().any()].tolist()
+            df[cols] = df[cols].fillna(df.mode().iloc[0])
+        
+        elif method == 'ffill':
+            # forward-fill
+            df = df.fillna(method='ffill')
+        
+        elif method == 'bfill':
+            # back-fill
+            df = df.fillna(method='bfill')
+        
+        elif method == 'drop':
+            df = pd.read_csv('bank-additional.csv', delimiter = ',')
+            df = df.replace(to_replace=['unknown'], value = np.NaN , regex = True)
+            df=df.dropna(axis=0, how='any',) 
+        convert_to_int(df, "y", "y") #Create a deposit int
+        convert_to_int(df, "housing", "housing") # Create housingint column
+        convert_to_int(df, "loan", "loan") #Create a loan_int column
+        convert_to_int(df, "default", "default") #Create a default_int column
+        c = Series.unique(df['contact'])
+        df['contact'] = label_binarize(df['contact'], classes=c)    
+
+    return df
 
 
 
-
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
